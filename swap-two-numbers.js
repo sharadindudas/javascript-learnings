cdbd21cb4d9900b5ad6b13ea75ac4639e72c4fe9
Sharadindu Das <sharadindudas774@gmail.com>
2000-01-20 23:12:00 +0530

    feat: Swap two numbers with or without third variables program added

--- swap-two-numbers.js
+++ swap-two-numbers.js
@@ -5,8 +5,17 @@ function swapTwoNumbers(a, b) {
     b = temp;
     return [a, b];
 }
+console.log(swapTwoNumbers(10, 20));
 
 function swapTwoNumbersJSWay(a, b) {
     return ([a, b] = [b, a]);
 }
 console.log(swapTwoNumbersJSWay(10, 20));
+
+function swapTwoNumbersWithoutThirdVar(a, b) {
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    return [a, b];
+}
+console.log(swapTwoNumbersWithoutThirdVar(10, 20));
