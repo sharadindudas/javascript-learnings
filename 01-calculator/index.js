d0f85c0f0b2e77c1722e7ce263ca5fb3021a882a
Sharadindu Das <sharadindudas774@gmail.com>
2025-09-11 00:07:49 +0530

    fix: Fixed the calculator logic

--- 01-calculator/index.js
+++ 01-calculator/index.js
@@ -1,39 +1,41 @@
 const buttons = document.querySelectorAll("button");
 const output = document.querySelector("#output");
 
-function isOperator(operator) {
-    return ["+", "-", "*", "/", "%"].includes(operator);
+function isOperator(value) {
+    return ["+", "-", "*", "/", "%"].includes(value);
 }
 
 buttons.forEach((btn) => {
     btn.addEventListener("click", (e) => {
         const currentButtonValue = e.target.value;
-        const lastCharacter = output.value.slice(0, -1);
+        const lastCharacter = output.value.slice(-1);
 
         switch (currentButtonValue) {
             case "AC":
                 output.value = "0";
                 break;
             case "DEL":
-                output.value = output.value.slice(0, -1);
+                output.value.length === 1
+                    ? (output.value = "0")
+                    : (output.value = output.value.slice(0, -1));
                 break;
             case "=":
                 try {
                     output.value = eval(output.value);
                 } catch (err) {
+                    console.error(err);
                     output.value = "Error";
                 }
                 break;
             default:
-                if (
-                    isOperator(currentButtonValue) &&
-                    isOperator(lastCharacter)
+                if (isOperator(currentButtonValue) && isOperator(lastCharacter))
+                    return;
+                else if (isOperator(currentButtonValue) && output.value === "0")
+                    return;
+                else if (
+                    output.value === "0" ||
+                    output.value.includes("Error")
                 ) {
-                    return;
-                }
-                if (isOperator(currentButtonValue) && output.value === "0") {
-                    return;
-                } else if (output.value === "0") {
                     output.value = currentButtonValue;
                 } else {
                     output.value += currentButtonValue;
