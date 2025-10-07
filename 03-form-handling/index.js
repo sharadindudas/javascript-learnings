f27d2b5c95dfea1e6619d5d474e0e996a2c54b5a
Sharadindu Das <sharadindudas774@gmail.com>
2025-10-07 23:51:46 +0530

    feat: Added the form handling logic

--- 03-form-handling/index.js
+++ 03-form-handling/index.js
@@ -1,33 +1,78 @@
 const form = document.querySelector("form");
-const errorMessage = document.querySelector(".error-message");
-const nameErrorMessage = document
-    .querySelector(".name-input")
-    .querySelector(".error-message");
-const emailErrorMessage = document
-    .querySelector(".email-input")
-    .querySelector(".error-message");
+const nameErrorMessage = document.querySelector(".name-input .error-message");
+const emailErrorMessage = document.querySelector(".email-input .error-message");
+const passwordErrorMessage = document.querySelector(
+    ".password-input .error-message"
+);
+const genderErrorMessage = document.querySelector(
+    ".gender-input .error-message"
+);
+const bioErrorMessage = document.querySelector(".bio-input .error-message");
+
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const passwordRegex =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    const name = e.target[0].value;
-    const email = e.target[1].value;
-    const password = e.target[2].value;
-    const gender = e.target[3].value;
-    const bio = e.target[4].value;
 
-    if (!name || name.length === 0) {
+    const name = form.querySelector("input[name='name']").value.trim();
+    const email = form.querySelector("input[name='email']").value.trim();
+    const password = form.querySelector("input[name='password']").value.trim();
+    const gender = form.querySelector("select[name='gender']").value.trim();
+    const bio = form.querySelector("textarea[name='bio']").value.trim();
+
+    nameErrorMessage.textContent = "";
+    emailErrorMessage.textContent = "";
+    passwordErrorMessage.textContent = "";
+    genderErrorMessage.textContent = "";
+    bioErrorMessage.textContent = "";
+
+    let hasError = false;
+
+    if (!name) {
         nameErrorMessage.textContent = "Please provide a name";
-        return;
+        hasError = true;
     } else if (name.length < 3) {
         nameErrorMessage.textContent = "Name must be at least 3 characters";
-        return;
+        hasError = true;
     } else if (name.length > 60) {
         nameErrorMessage.textContent = "Name must not exceed 60 characters";
-        return;
-    } else if (!email || email.length === 0) {
-        emailErrorMessage.textContent = "Please provide an email";
-        return;
+        hasError = true;
     }
 
-    // form.reset();
+    if (!email) {
+        emailErrorMessage.textContent = "Please provide an email";
+        hasError = true;
+    } else if (!emailRegex.test(email)) {
+        emailErrorMessage.textContent = "Please provide a valid email address";
+        hasError = true;
+    }
+
+    if (!password) {
+        passwordErrorMessage.textContent = "Please provide a password";
+        hasError = true;
+    } else if (!passwordRegex.test(password)) {
+        passwordErrorMessage.textContent =
+            "Password must have least 8 characters long and contains at least one lowercase letter, one uppercase letter, one digit, and one special character";
+        hasError = true;
+    }
+
+    if (!gender) {
+        genderErrorMessage.textContent = "Please provide a gender";
+        hasError = true;
+    } else if (!["male", "female"].includes(gender)) {
+        genderErrorMessage.textContent =
+            "Please choose the gender from the following options";
+        hasError = true;
+    }
+
+    if (bio && bio.length > 300) {
+        bioErrorMessage.textContent = "Bio must not exceed 300 characters";
+        hasError = true;
+    }
+
+    if (hasError) return;
+    alert("Form submitted successfully!");
+    form.reset();
 });
