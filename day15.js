eac6833df2b046a2fde9277054a6f980b4aa2382
Sharadindu Das <sharadindudas774@gmail.com>
2026-01-15 23:32:23 +0530

    chore: Added string palindrome and power (base exponent program)

--- day15.js
+++ day15.js
@@ -22,7 +22,7 @@ function getLargestNumber(input) {
     }
     return largestNumber;
 }
-console.log(getLargestNumber([1, 5, 6, 3]));
+// console.log(getLargestNumber([1, 5, 6, 3]));
 
 // Write a function to check if a string is palindrome or not
 function isStringPalindrome(input) {
@@ -30,19 +30,42 @@ function isStringPalindrome(input) {
         throw new Error("Please provide a string");
     }
 
-    const modifiedInput = input.toLowerCase().replace(/[^a-z0-9]/g, "");
+    const str = input.toLowerCase().replace(/[^a-z0-9]/g, "");
 
-    const copiedStr = modifiedInput;
     let output = "";
-    for (let i = modifiedInput.length - 1; i >= 0; i--) {
-        output += modifiedInput[i];
+    for (let i = str.length - 1; i >= 0; i--) {
+        output += str[i];
     }
 
-    console.log(output, copiedStr);
-    return output === copiedStr;
+    return output === str;
 }
+
+function isStringPalindrome(input) {
+    if (typeof input !== "string") {
+        throw new Error("Please provide a string");
+    }
+    const str = input.toLowerCase().replace(/[^a-z0-9]/g, "");
+    const reversedStr = str.split("").reverse().join("");
+    return str === reversedStr;
+}
+
+function isStringPalindrome(input) {
+    if (typeof input !== "string") {
+        throw new Error("Please provide a string");
+    }
+    const str = input.toLowerCase().replace(/[^a-z0-9]/g, "");
+    let left = 0,
+        right = str.length - 1;
+
+    while (left < right) {
+        if (str[left] !== str[right]) return false;
+        left++;
+        right--;
+    }
+    return true;
+}
+
 console.log(isStringPalindrome("madam"));
 console.log(isStringPalindrome("level"));
-console.log(isStringPalindrome("asd"));
 console.log(isStringPalindrome("Madam"));
 console.log(isStringPalindrome("nurses run"));
