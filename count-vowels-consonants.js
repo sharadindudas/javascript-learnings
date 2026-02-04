ce5e89355fc3655a5374ff958c89d06b02485b1a
Sharadindu Das <sharadindudas774@gmail.com>
2026-02-04 07:32:48 +0530

    fix: Some code logic fixes

--- count-vowels-consonants.js
+++ count-vowels-consonants.js
@@ -1,28 +1,42 @@
 // Write a function that counts and prints the number of vowels and consonants in a given string
+// function countVowelsAndConsonants(input) {
+//     if (typeof input !== "string") {
+//         throw new Error("Please provide a string");
+//     }
+//     input = input.toLowerCase();
+
+//     const output = { vowels: 0, consonants: 0 };
+//     const vowels = ["a", "e", "i", "o", "u"];
+//     for (let i = 0; i < input.length; i++) {
+//         const char = input[i];
+//         if (char >= "a" && char <= "z") {
+//             if (vowels.includes(char)) output.vowels++;
+//             else output.consonants++;
+//         }
+//     }
+//     return output;
+// }
+
+// Better solution
 function countVowelsAndConsonants(input) {
     if (typeof input !== "string") {
-        throw new Error("Input must be a string");
+        throw new Error("Please provide a string");
     }
+    input = input.toLowerCase();
 
-    const validInput = input.toLowerCase().replace(/[^a-z]/g, "");
+    const output = { vowels: 0, consonants: 0 };
     const vowels = new Set(["a", "e", "i", "o", "u"]);
-
-    let vowelsCount = 0,
-        consonantsCount = 0;
-
-    for (let i = 0; i < validInput.length; i++) {
-        const value = validInput[i];
-        if (vowels.has(value)) {
-            vowelsCount++;
-        } else {
-            consonantsCount++;
+    for (let i = 0; i < input.length; i++) {
+        const char = input[i];
+        if (char >= "a" && char <= "z") {
+            if (vowels.has(char)) output.vowels++;
+            else output.consonants++;
         }
     }
-
-    return {
-        vowelsCount,
-        consonantsCount,
-    };
+    return output;
 }
 console.log(countVowelsAndConsonants("Hello world"));
 console.log(countVowelsAndConsonants("12345"));
+console.log(countVowelsAndConsonants("123"));
+console.log(countVowelsAndConsonants("!!!"));
+console.log(countVowelsAndConsonants(" "));
--- prime-checker.js
+++ prime-checker.js
@@ -8,8 +8,6 @@ function checkPrimeNumber(input) {
         return false;
     }
 
-    console.log(Math.sqrt(input))
-
     for (let i = 2; i <= Math.sqrt(input); i++) {
         if (input % i === 0) {
             return false;
